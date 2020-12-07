e6f3a00966aefdc25cbcd5fb9791a93d9e468b5c
= <dangnt520@wru.vn>
2020-12-07 17:13:51 +0700

    [Dijktra] V1 - Upgrade

--- src/main/java/algorithm/dijktra/ImplementDijktraAlgorithm.java
+++ src/main/java/algorithm/dijktra/ImplementDijktraAlgorithm.java
@@ -11,22 +11,24 @@ public class ImplementDijktraAlgorithm {
   private Queue<Node> queue = new ArrayDeque<>();
 
 
-  public Integer getMin(String sourceNode, String targetNode, Map<String, Node> nodes){
+  public Integer getMin(String sourceNode, String target, Map<String, Node> nodes){
     //For start;
     Node startNode = nodes.get(sourceNode);
     startNode.setMinValue(0);
+
+    Node targetNode = nodes.get(target);
     queue.add(startNode);
     //While queue is not empty. Find min.
     while(!queue.isEmpty()){
       Node currentNode = queue.poll();
-      fromANode(currentNode);
+      fromANode(currentNode, targetNode );
       //set node is visited. For ignore.
       currentNode.setVisited(true);
     }
 
-    Integer result = nodes.get(targetNode).getMinValue();
+    Integer result = nodes.get(target).getMinValue();
     System.out.println("Min: " +result);
-    System.out.println(new StringBuilder("Path: " + nodes.get(targetNode).getPath(sourceNode)).reverse().toString() );
+    System.out.println("Path: " + new StringBuilder(nodes.get(target).getPath(sourceNode)).reverse().toString() );
     return result;
   }
 
@@ -35,6 +37,34 @@ public class ImplementDijktraAlgorithm {
       return;
     }
     if(node.visited()){
+      return;
+    }
+    //fill min value to next node.
+    for(Entry<Node, Integer> entryNode : node.getAdjacentNodes().entrySet()){
+      Node nextNode = entryNode.getKey();
+      //if node was visited. Ignore
+      if(nextNode.visited()){
+        continue;
+      }
+      Integer calculateMin = node.getMinValue() + entryNode.getValue();
+      //if minValue is not fill or calculateMin < currentMin => Set new minValue
+      if(nextNode.getMinValue() == null || nextNode.getMinValue() > calculateMin){
+        nextNode.setMinValue(calculateMin);
+        nextNode.setPreviousNode(node);
+        queue.add(nextNode);
+      }
+    }
+  }
+
+  private void fromANode(Node node, Node targetNode){
+    if(node == null){
+      return;
+    }
+    if(node.visited()){
+      return;
+    }
+    //ignore if currentNode have minValue great than targetNode. Don't need to check anymore.
+    if(targetNode.getMinValue() != null && node.getMinValue() > targetNode.getMinValue()){
       return;
     }
     //fill min value to next node.
